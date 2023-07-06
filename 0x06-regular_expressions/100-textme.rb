f8f1a02f2c441f217de130664ea4349b81c88f0d
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-06 13:36:15 -0700

    update

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,9 @@
 #!/usr/bin/env ruby
 
+log_entries = ARGV[0]
+
 regex = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
 
-ARGF.each do |line|
-  match = line.match(regex)
-  if match
-    sender = match[1]
-    receiver = match[2]
-    flags = match[3]
-    puts "#{sender},#{receiver},#{flags}"
-  end
+log_entries.scan(regex) do |sender, receiver, flags|
+  puts "#{sender},#{receiver},#{flags}"
 end
