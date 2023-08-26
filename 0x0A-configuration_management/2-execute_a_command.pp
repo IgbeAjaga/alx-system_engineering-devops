2a3a18df842ce830f1988056e137f739c1d79e40
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-26 03:51:53 -0700

    correction of output

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,14 @@
-# Kills the process named killmenow using pkill
+# Execute a command to kill a process named killmenow
+
+# Execute the pkill command to terminate the process
 exec { 'killmenow':
-  command => 'pkill -f killmenow',
-  onlyif  => 'pgrep -f killmenow',
+  command     => 'pkill -f killmenow',
+  onlyif      => 'pgrep -f killmenow',
+  path        => '/bin:/usr/bin',
+  refreshonly => true,
+}
+
+# Notify to print "OK" when the process is successfully terminated
+notify { 'OK':
+  subscribe => Exec['killmenow'],
 }
