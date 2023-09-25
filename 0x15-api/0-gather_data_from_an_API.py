e2c3f8feae49fb1d082c5de1fcd59d1b164f748a
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-25 11:10:59 -0700

    API data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This script fetches data from a REST API and displays the employee's TODO list progress.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit("Usage: {} <employee_id>".format(sys.argv[0]))
+
+    employee_id = sys.argv[1]
+
+    # Fetch user data
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    response_user = requests.get(user_url)
+    user_data = response_user.json()
+    employee_name = user_data.get('name')
+
+    # Fetch TODO data
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
+    response_todo = requests.get(todo_url)
+    todo_list = response_todo.json()
+
+    # Calculate the number of completed tasks
+    total_tasks = len(todo_list)
+    completed_tasks = sum(1 for task in todo_list if task.get('completed'))
+
+    # Display the result
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
+    for task in todo_list:
+        if task.get('completed'):
+            print("\t {}".format(task.get('title')))
