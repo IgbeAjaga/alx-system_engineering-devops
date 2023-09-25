ede06e7896b6bb4010e0b3ffa0f4f3e7e9124fcb
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-25 12:10:56 -0700

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,9 +8,6 @@ import csv
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit("Usage: {} <employee_id>".format(sys.argv[0]))
-
     employee_id = sys.argv[1]
 
     # Fetch user data
